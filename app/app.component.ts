a26a685ed9ffe4166422475e2efe60c69e9d4517
sonamkukkar <sonamkukkar@gmail.com>
2018-03-29 13:03:42 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle:string = 'Product Management';
+}
---
+++ app/shared/convert-to-spaces.pipe.js
@@ -0,0 +1,24 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+var module_1 = require();
+/core;;
+var ConvertToSpacesPipe = /** @class */ (function () {
+    function ConvertToSpacesPipe() {
+    }
+    ConvertToSpacesPipe.prototype.transform = function (value, character) {
+        return value.replace(character, ' ');
+    };
+    ConvertToSpacesPipe = __decorate([
+        module_1.Pipe({
+            name: 'ConvertToSpaces'
+        })
+    ], ConvertToSpacesPipe);
+    return ConvertToSpacesPipe;
+}());
+exports.ConvertToSpacesPipe = ConvertToSpacesPipe;
